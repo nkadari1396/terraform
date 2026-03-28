0559a3a9f6babb390ed7f3c943a39e8583ef9eb8
Naveen Kadari <nkadari1396@gmail.com>
2026-03-28 14:47:12 +0800

    terraform

--- variables/ec2.tf
+++ variables/ec2.tf
@@ -3,8 +3,8 @@ resource "aws_instance" "terraform" {
   instance_type = var.instance_type
   vpc_security_group_ids = [ "aws_security_group.allow_sshh.id"]
 
-  tags = var.t
-  
+  tags = var.tags
+
 }
 
 
