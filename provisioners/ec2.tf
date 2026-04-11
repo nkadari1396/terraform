c673174917cf2506bdbfef61732e06f95d5c188e
Naveen Kadari <nkadari1396@gmail.com>
2026-04-11 14:36:31 +0800

    provisioners

---
+++ provisioners/ec2.tf
@@ -0,0 +1,64 @@
+resource "aws_instance" "expense" {
+    for_each = var.instances #this variable is map
+    #for_each will give us a special variable with name for_each
+    ami = "ami-0220d79f3f480ecf5"
+    instance_type = each.value
+    vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
+    tags ={
+        Name = each.key
+    }
+
+    provisioner "local-exec" {
+        command = "echo ${self.private_ip} > public_ip.txt"
+    }
+
+    connection {
+        type     = "ssh"
+        user     = "ec2-user"
+        password = "DevOps321"
+        host     = self.public_ip
+    }
+    
+    # provisioners will execute at the time of creation
+    provisioner "remote-exec" {
+        inline = [
+            "sudo dnf install ansible -y",
+            "sudo dnf install nginx -y",
+            "sudo systemctl start nginx",
+        ]
+    }
+
+    provisioner "remote-exec" {
+        when    = destroy
+        inline = [
+            "sudo systemctl stop nginx",
+        ]
+    }
+
+}
+
+resource "aws_security_group" "allow_ssh_terraform" {
+    name = "allow_sshh"
+    description = "Allow port number 22 for SSH access"
+
+    #usually we allow everything in egress
+        egress {
+            from_port = 0
+            to_port = 0
+            protocol = "-1"
+            cidr_blocks = ["0.0.0.0/0"]
+            ipv6_cidr_blocks = ["::/0"]
+        }
+
+        ingress {
+            from_port = 22
+            to_port = 22
+            protocol = "tcp"
+            cidr_blocks = ["0.0.0.0/0"] #allow from everyone
+            ipv6_cidr_blocks = ["::/0"]
+        }
+
+        tags = {
+            Name = "allow_sshh"
+        }
+}
---
+++ provisioners/variables.tf
@@ -0,0 +1,17 @@
+variable "instances" {
+    type = map
+    default = {
+        mysql = "t3.small"
+        backend = "t3.micro"
+        frontend = "t3.micro"
+    }
+}
+
+variable "domain_name" {
+    default = "naveenkadari.com"
+}
+
+variable "zone_id" {
+    default = "Z05521101U68XI4NW84J2"
+}
+    
